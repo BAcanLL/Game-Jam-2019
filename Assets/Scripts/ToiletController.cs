fb019aa5cf2948620fa86b9b82a70e9859d8ffac
Chris <chrisfjski@gmail.com>
2019-01-26 21:43:47 -0500

    Fixed ToiletController class name to allow attachment to sprites

--- Assets/Scripts/ToiletController.cs
+++ Assets/Scripts/ToiletController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Toilet : InteractiveController
+public class ToiletController : InteractiveController
 {
     private Timer overflowTimer;
     private Timer plungeTimer;
