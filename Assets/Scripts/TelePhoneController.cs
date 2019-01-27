8b5be45ff91ad440a59eef82c8d8cd7699a64798
Mac <soniciceman@gmail.com>
2019-01-26 22:43:52 -0500

    clean up stuff

--- Assets/Scripts/TelePhoneController.cs
+++ Assets/Scripts/TelePhoneController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 public class TelePhoneController : InteractiveController {
  
-    private Rigidbody2D pRBody;
     private Timer nextCallTimer;
     private float nextCallTime;
+    Animator anim;
+
 	// Use this for initialization
 	void Start () {
-        Init(transitionOffTime: 2, key:KeyCode.Space);
-        nextCallTime = Random.Range(15, 45);
+        Init(/*transitionOffTime: 2,*/ key:KeyCode.Space);
+        nextCallTime = Random.Range(3, 5);
         nextCallTimer = new Timer(nextCallTime);
+        anim = GetComponent<Animator>();
         Debug.Log("TIME TIL NEXT CALL: " + nextCallTime);
 	}
 
@@ -19,21 +21,33 @@ public class TelePhoneController : InteractiveController {
     {
         base.Update();
         // Debug.Log(state);
-	}
+
+        if (state == State.TransitionOn)
+        {
+        }
+        if (state == State.On)
+        {
+            anim.Play("On");
+        }
+        if (state == State.Off)
+        {
+            anim.Play("Off");
+        }
+    }
     public override void UpdateOff()
     {
         // Only activates once, switches us to an activating state
         nextCallTimer.Update();
         // Debug.Log(nextCallTimer.time.ToString());
         if(nextCallTimer.Done) {
-            state = State.TransitionOn;
+            state = State.On;
         }
     }
-    public override void UpdateTransitionOn()
-    {
-        // pass
-        state = State.On;
-    }
+    //public override void UpdateTransitionOn()
+    //{
+    //    // pass
+    //    state = State.On;
+    //}
 
     public override void UpdateTransitionOff()
     {
